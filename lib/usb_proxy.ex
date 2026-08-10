e43bb31d51095960e7b9ca161fc2512c0ed7c6b7
Lars Wikman <lars@underjord.io>
2026-08-10 17:17:09 +0200

    Initial commit

---
+++ lib/usb_proxy.ex
@@ -0,0 +1,18 @@
+defmodule UsbProxy do
+  @moduledoc """
+  Documentation for `UsbProxy`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> UsbProxy.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/usb_proxy_test.exs
@@ -0,0 +1,8 @@
+defmodule UsbProxyTest do
+  use ExUnit.Case
+  doctest UsbProxy
+
+  test "greets the world" do
+    assert UsbProxy.hello() == :world
+  end
+end
